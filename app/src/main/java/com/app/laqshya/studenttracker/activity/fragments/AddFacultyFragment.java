e3c3e96605a83677b7956b81032bc223475ce675
bismeet <bismeetsingh352@gmail.com>
2018-07-07 16:04:08 +0530

    Fixed Counsellor Registration

--- app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/AddFacultyFragment.java
+++ app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/AddFacultyFragment.java
@@ -1,5 +1,6 @@
 package com.app.laqshya.studenttracker.activity.fragments;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,17 +8,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.app.laqshya.studenttracker.R;
+import com.app.laqshya.studenttracker.databinding.RegisterFacultyBinding;
 
 public class AddFacultyFragment extends Fragment {
+    private RegisterFacultyBinding registerFacultyBinding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.register_faculty,container,false);
-        return view;
+        registerFacultyBinding = DataBindingUtil.inflate(inflater, R.layout.register_faculty, container, false);
+        return registerFacultyBinding.getRoot();
     }
 }
---
+++ app/src/main/java/com/app/laqshya/studenttracker/activity/model/CourseList.java
@@ -0,0 +1,14 @@
+package com.app.laqshya.studenttracker.activity.model;
+
+public class CourseList {
+    private String course_name;
+
+    public CourseList(String course_name) {
+
+        this.course_name = course_name;
+    }
+
+    public String getCourse_name() {
+        return course_name;
+    }
+}
