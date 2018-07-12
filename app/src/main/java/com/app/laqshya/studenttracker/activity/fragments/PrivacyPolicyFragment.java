664f1210d40a7c3df74886463ad770cf2eb6fdf3
laqshyainfosoftwinrwinter420git config --global --unset user.nameclrit config --global user.name Mona <laqshyainfosoft@gmail.com>
2018-07-12 16:38:50 +0530

    Commit_RS

--- app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/PrivacyPolicyFragment.java
+++ app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/PrivacyPolicyFragment.java
@@ -1,5 +1,8 @@
 package com.app.laqshya.studenttracker.activity.fragments;
 
+import android.annotation.SuppressLint;
+import android.databinding.DataBindingUtil;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +10,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import com.app.laqshya.studenttracker.R;
+import com.app.laqshya.studenttracker.activity.utils.Constants;
+import com.app.laqshya.studenttracker.databinding.FragmentPaymentBinding;
 
 public class PrivacyPolicyFragment extends Fragment {
-    @Nullable
-    @Override
+
+    FragmentPaymentBinding fragmentPaymentBinding;
+
+    ProgressBar progressBar;
+
+
+    @SuppressLint("SetJavaScriptEnabled")
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        fragmentPaymentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_payment, container, false);
+        fragmentPaymentBinding.webview.loadUrl(Constants.WEBSITE_URL);
+        fragmentPaymentBinding.webview.setWebViewClient(new PrivacyPolicyFragment.myWebClient());
+        progressBar = fragmentPaymentBinding.progressBar;
+        WebSettings webSettings = fragmentPaymentBinding.webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        return fragmentPaymentBinding.getRoot();
+    }
+
+    public class myWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            progressBar.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
     }
 }
