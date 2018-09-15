f73bd534cc52bb7dae4def1a241949cd5627b27c
rashil <rashil1993@gmail.com>
2018-09-15 15:20:10 +0530

    lasdjl

--- app/src/main/java/com/app/laqshya/studenttracker/activity/service/EduTrackerService.java
+++ app/src/main/java/com/app/laqshya/studenttracker/activity/service/EduTrackerService.java
@@ -23,7 +23,7 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface EduTrackerService {
-    String ENDPOINT = "http://192.168.0.130/student_tracker/";
+    String ENDPOINT = "http://192.168.0.128/student_tracker/";
 
     @FormUrlEncoded
     @POST("login.php")
