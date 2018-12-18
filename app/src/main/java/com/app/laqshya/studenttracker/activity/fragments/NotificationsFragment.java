8018eaeae1f0b09cbc2133fd6dd3fd0731ae7d95
bismeet <bismeetsingh352@gmail.com>
2018-12-18 12:29:26 +0530

    Added Navigation Drawer Delete.

--- app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/NotificationsFragment.java
+++ app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/NotificationsFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.app.laqshya.studenttracker.databinding.FragmentBroadcastAllFacultyBinding;
 import com.app.laqshya.studenttracker.databinding.FragmentHomeAdminBinding;
@@ -15,15 +16,14 @@ import com.app.laqshya.studenttracker.databinding.FragmentNotificationBinding;
 import java.util.List;
 
 import timber.log.Timber;
-    
+
 public class NotificationsFragment extends Fragment {
     FragmentNotificationBinding fragmentNotificationBinding;
+    TextView textView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Timber.d("onCreated");
-        fragmentNotificationBinding = FragmentNotificationBinding.inflate(inflater, container, false);
         return fragmentNotificationBinding.getRoot();
     }
 }
