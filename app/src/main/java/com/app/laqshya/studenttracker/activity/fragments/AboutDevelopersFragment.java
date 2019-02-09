2bf35722828a151b16525d1c217b2f28032137fc
Akshay Sawant <akshaysawant003@gmail.com>
2019-02-08 23:28:18 -0800

    Updated files are-
    *Java Files
    1. AboutDevelopersFragment
    2. ContactFragment
    3. HomeFragmentAdmin
    Refer_FriendFragment
    
    *Drawable Files
    1. facebook_logo
    2. internet
    3. laqshya_institute_logo
    4. location_logo
    5. mail_logo
    6. mobile_icon
    7. refer_a_friend
    8. telephone_icon
    9. whatsapp_logo
    
    * Layout files
    1. about_developers
    2. fragmnet_contact
    3. fragment_refer_friend
    
    *others
    1. strings.xml
    2. build.gradle

--- app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/AboutDevelopersFragment.java
+++ app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/AboutDevelopersFragment.java
@@ -1,5 +1,6 @@
 package com.app.laqshya.studenttracker.activity.fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -10,68 +11,142 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.laqshya.studenttracker.R;
 
-public class AboutDevelopersFragment extends Fragment {
+public class AboutDevelopersFragment extends Fragment implements View.OnClickListener {
 
-    LinearLayout facebook, whatsapp;
-    TextView mail, website;
+    TextView mEmailTitle, mEmailId, mMobileTitle, mMobileNo, mWhatsappTitle, mWhatsappNo,
+            mFacebookTitle, mFacebook, mWebsiteTitle, mWebsiteLink, mAddressTitle, mAddress;
+
+    Intent mCallIntent;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View rootview = inflater.inflate(R.layout.about_developers, container, false);
-        mail = rootview.findViewById(R.id.email);
-        website = rootview.findViewById(R.id.website);
-        facebook = rootview.findViewById(R.id.facebook);
-        whatsapp = rootview.findViewById(R.id.whatsapp);
-        mail.setOnClickListener(view -> {
-            Toast.makeText(getActivity(), "Please wait...", Toast.LENGTH_SHORT).show();
-            Intent localIntent4 = new Intent("android.intent.action.SEND");
-            localIntent4.setData(Uri.parse(":mailto"));
-            localIntent4.setType("message/rfc822");
-            localIntent4.putExtra("android.intent.extra.EMAIL", new String[]{"info@laqshyainfosoft.com"});
-            startActivity(Intent.createChooser(localIntent4, "Mail us"));
-        });
 
-        website.setOnClickListener(view -> {
-            Intent localIntent2 = new Intent("android.intent.action.VIEW");
-            localIntent2.setData(Uri.parse("http://www.laqshyainfosoft.com"));
-            startActivity(localIntent2);
-        });
+        //Binding the views
+        mEmailTitle = rootview.findViewById(R.id.email_title);
+        mEmailId = rootview.findViewById(R.id.email_id);
+        mMobileTitle = rootview.findViewById(R.id.mobile_title);
+        mMobileNo = rootview.findViewById(R.id.mobile_no);
+        mWhatsappTitle = rootview.findViewById(R.id.whatsapp_title);
+        mWhatsappNo = rootview.findViewById(R.id.whatsapp_no);
+        mFacebookTitle = rootview.findViewById(R.id.facebook_title);
+        mFacebook = rootview.findViewById(R.id.facebook_id);
+        mWebsiteTitle = rootview.findViewById(R.id.website_title);
+        mWebsiteLink = rootview.findViewById(R.id.website_link);
+        mAddressTitle = rootview.findViewById(R.id.address_title);
+        mAddress = rootview.findViewById(R.id.address);
 
-        facebook.setOnClickListener(view -> {
-            Intent localIntent3 = new Intent("android.intent.action.VIEW");
-            localIntent3.setData(Uri.parse("https://www.facebook.com/Laqshya-Infosoft-Solutions-1789618061277540/?fref=ts"));
-            startActivity(localIntent3);
-        });
-
-        whatsapp.setOnClickListener(view -> {
-            Intent sendIntent = new Intent(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "Laqshya Infosoft Solutions....Please feel free to contact to know more about US" +
-                    "\n" + "http://www.laqshyainfosoft.com/");
-            sendIntent.setType("text/plain");
-            sendIntent.setPackage("com.whatsapp");
-            startActivity(sendIntent);
-        });
-
+        //Setting onClick listener
+        mEmailTitle.setOnClickListener(this);
+        mEmailId.setOnClickListener(this);
+        mMobileTitle.setOnClickListener(this);
+        mMobileNo.setOnClickListener(this);
+        mWhatsappTitle.setOnClickListener(this);
+        mWhatsappNo.setOnClickListener(this);
+        mFacebookTitle.setOnClickListener(this);
+        mFacebook.setOnClickListener(this);
+        mWebsiteTitle.setOnClickListener(this);
+        mWebsiteLink.setOnClickListener(this);
+        mAddressTitle.setOnClickListener(this);
+        mAddress.setOnClickListener(this);
 
         return rootview;
     }
-
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.email_title:
+                emailFunc();
+                break;
+            case R.id.email_id:
+                emailFunc();
+                break;
+            case R.id.mobile_title:
+                callFunc();
+                break;
+            case R.id.mobile_no:
+                callFunc();
+                break;
+            case R.id.whatsapp_title:
+                whatsappFunc();
+                break;
+            case R.id.whatsapp_no:
+                whatsappFunc();
+                break;
+            case R.id.facebook_title:
+                facebookFunc();
+                break;
+            case R.id.facebook_id:
+                facebookFunc();
+                break;
+            case R.id.website_title:
+                websiteFunc();
+                break;
+            case R.id.website_link:
+                websiteFunc();
+                break;
+            case R.id.address_title:
+                addressFunc();
+                break;
+            case R.id.address:
+                addressFunc();
+                break;
+        }
+    }
 
+    @SuppressLint("IntentReset")
+    public void emailFunc() {
+        Toast.makeText(getActivity(), "Please wait...", Toast.LENGTH_SHORT).show();
+        mCallIntent = new Intent("android.intent.action.SEND");
+        mCallIntent.setData(Uri.parse(":mailto"));
+        mCallIntent.setType("message/rfc822");
+        mCallIntent.putExtra("android.intent.extra.EMAIL", new String[]{"lalaqshya@gmail.com"});
+        startActivity(Intent.createChooser(mCallIntent, "Mail us"));
+    }
+
+    public void callFunc() {
+        mCallIntent = new Intent(Intent.ACTION_DIAL);
+        mCallIntent.setData(Uri.parse("tel:" + "1234567890"));
+    }
+
+    public void whatsappFunc() {
+        mCallIntent = new Intent(Intent.ACTION_SEND);
+        mCallIntent.putExtra(Intent.EXTRA_TEXT, "Laqshya Infosoft Solutions....Please feel free to contact to know more about US" +
+                "\n" + "http://www.laqshyainfosoft.com/");
+        mCallIntent.setType("text/plain");
+        mCallIntent.setPackage("com.whatsapp");
+        startActivity(mCallIntent);
+    }
+
+    public void facebookFunc() {
+        mCallIntent = new Intent("android.intent.action.VIEW");
+        mCallIntent.setData(Uri.parse("https://www.facebook.com/Laqshya-Infosoft-Solutions-1789618061277540/?fref=ts"));
+        startActivity(mCallIntent);
+    }
+
+    public void websiteFunc() {
+        mCallIntent = new Intent("android.intent.action.VIEW");
+        mCallIntent.setData(Uri.parse("http://www.laqshyainfosoft.com"));
+        startActivity(mCallIntent);
+    }
+
+    public void addressFunc() {
+        mCallIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("geo:19.1205157,72.8447169"));
+        startActivity(mCallIntent);
+    }
 }
 
--- app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/Refer_Friendfragment.java
+++ app/src/main/java/com/app/laqshya/studenttracker/activity/fragments/Refer_Friendfragment.java
@@ -1,6 +1,107 @@
 package com.app.laqshya.studenttracker.activity.fragments;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Refer_Friendfragment extends Fragment {
+import com.app.laqshya.studenttracker.R;
+
+public class Refer_Friendfragment extends Fragment implements View.OnClickListener {
+
+    ImageView mWhatsappImg, mFacebookImg, mGmailImg;
+    Button mWhatsapp, mFacebook, mGmail;
+    Intent mSocialMediaIntent;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootview = inflater.inflate(R.layout.fragment_refer_friend, container, false);
+
+        //binding social media icons
+        mWhatsappImg = rootview.findViewById(R.id.whatsapp_icon);
+        mFacebookImg = rootview.findViewById(R.id.fb_icon);
+        mGmailImg = rootview.findViewById(R.id.gmail_icon);
+
+        //Binding social media buttons
+        mWhatsapp = rootview.findViewById(R.id.whatsapp_reference);
+        mFacebook = rootview.findViewById(R.id.fb_reference);
+        mGmail = rootview.findViewById(R.id.gmail_reference);
+
+        //Calling event listeners for social media icons
+        mWhatsappImg.setOnClickListener(this);
+        mFacebookImg.setOnClickListener(this);
+        mGmailImg.setOnClickListener(this);
+
+        //Calling event listeners for social media buttons
+        mWhatsapp.setOnClickListener(this);
+        mFacebook.setOnClickListener(this);
+        mGmail.setOnClickListener(this);
+
+        return rootview;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.whatsapp_icon:
+                callWhatsappFunc();
+                break;
+            case R.id.whatsapp_reference:
+                callWhatsappFunc();
+                break;
+            case R.id.fb_icon:
+                callFacebookFunc();
+                break;
+            case R.id.fb_reference:
+                callFacebookFunc();
+                break;
+            case R.id.gmail_icon:
+                callGmailFunc();
+                break;
+            case R.id.gmail_reference:
+                callGmailFunc();
+                break;
+        }
+    }
+
+    public void callWhatsappFunc() {
+        mSocialMediaIntent = new Intent(Intent.ACTION_SEND);
+        mSocialMediaIntent.putExtra(Intent.EXTRA_TEXT, "Laqshya Infosoft Solutions....Please feel free to contact to know more about US" +
+                "\n" + "http://www.laqshyainfosoft.com/");
+        mSocialMediaIntent.setType("text/plain");
+        mSocialMediaIntent.setPackage("com.whatsapp");
+        startActivity(mSocialMediaIntent);
+    }
+
+    public void callFacebookFunc() {
+        mSocialMediaIntent = new Intent("android.intent.action.VIEW");
+        mSocialMediaIntent.setData(Uri.parse("https://www.facebook.com/Laqshya-Infosoft-Solutions-1789618061277540/?fref=ts"));
+        startActivity(mSocialMediaIntent);
+    }
+
+    @SuppressLint("IntentReset")
+    public void callGmailFunc() {
+        mSocialMediaIntent = new Intent("android.intent.action.SEND");
+        mSocialMediaIntent.setData(Uri.parse(":mailto"));
+        mSocialMediaIntent.setType("message/rfc822");
+        mSocialMediaIntent.putExtra("android.intent.extra.EMAIL", new String[]{"info@laqshyainfosoft.com"});
+        startActivity(Intent.createChooser(mSocialMediaIntent, "Mail us"));
+    }
 }
